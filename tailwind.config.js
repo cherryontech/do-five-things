d213e12ec8b5ad1bf94b84f882950d7c8e64e61b
mash-a <mariya.mego@finditparts.com>
2024-06-23 12:00:09 -0700

    vite tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './app/helpers/**/*.rb',
+    './app/assets/stylesheets/**/*.css',
+    './app/views/**/*.{html,html.erb,erb}',
+    './app/javascript/components/**/*.js',
+  ],
+  theme: {
+    extend: {
+    },
+  },
+  corePlugins: {
+    aspectRatio: false,
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/line-clamp'),
+  ],
+}
