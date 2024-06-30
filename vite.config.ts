766e6fa806bcd0e9915922f66e7d09e6f9d6c7b4
mash-a <mariya.mego@finditparts.com>
2024-06-29 21:59:09 -0700

    remove unused js

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite'
 import FullReload from "vite-plugin-full-reload"
 import RubyPlugin from 'vite-plugin-ruby'
-import StimulusHMR from 'vite-plugin-stimulus-hmr'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   clearScreen: false,
   plugins: [
     RubyPlugin(),
-    StimulusHMR(),
     vue(),
     FullReload(["config/routes.rb", "app/views/**/*"], { delay: 300 }),
   ],
