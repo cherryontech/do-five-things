3771e9b5f409b5d6a8a48f311e83fe8a82785c46
novellac <38117965+novellac@users.noreply.github.com>
2024-07-06 13:01:14 -0400

    feat: Automatically add layouts to pages

--- app/frontend/entrypoints/inertia.js
+++ app/frontend/entrypoints/inertia.js
@@ -1,6 +1,8 @@
 import { createInertiaApp } from '@inertiajs/vue3'
 import { createApp, h } from 'vue'
 
+import BaseLayout from '../layouts/BaseLayout.vue'
+
 createInertiaApp({
   // Set default page title
   // see https://inertia-rails.netlify.app/guide/title-and-meta
@@ -14,15 +16,9 @@ createInertiaApp({
 
   resolve: (name) => {
     const pages = import.meta.glob('../pages/**/*.vue', { eager: true })
-    return pages[`../pages/${name}.vue`]
-
-    // To use a default layout, import the Layout component
-    // and use the following lines.
-    // see https://inertia-rails.netlify.app/guide/pages#default-layouts
-    //
-    // const page = pages[`./pages/${name}.vue`]
-    // page.default.layout ||= (page) => createElement(Layout, null, page)
-    // return page
+    let page = pages[`../pages/${name}.vue`]
+    page.default.layout = page.default.layout || BaseLayout
+    return page
   },
 
   setup({ el, App, props, plugin }) {
---
+++ app/frontend/layouts/BaseLayout.vue
@@ -0,0 +1,22 @@
+<template>
+  <main class="flex flex-col min-h-screen bg-dft-grey">
+    <header class="border-4">
+      <a href="#footerNav">Skip to nav (TODO: Hide until focused with keyboard)</a>
+    </header>
+    <article class="grow">
+
+      <slot />
+    </article>
+    <footer id="footerNav" class="bg-dft-secondary">
+      <Link href="/settings">Settings</Link>
+      <!-- TODO: Add route for /today -->
+      <Link href="https://example.com/today">Today</Link>
+      <!-- TODO: Add route for /progress -->
+      <Link href="https://example.com/progress">Progress</Link>
+    </footer>
+  </main>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3'
+</script>
