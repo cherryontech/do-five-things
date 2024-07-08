62329e49377fa79030e1c22dc5e9bf17e36a35e6
novellac <38117965+novellac@users.noreply.github.com>
2024-07-08 09:59:02 -0400

    feat: Add and use vue-i18n for UI text

--- app/frontend/pages/SettingsPage.vue
+++ app/frontend/pages/SettingsPage.vue
@@ -1,8 +1,18 @@
 <template>
   <div>
-   <h1>Settings page (TODO: Refactor into a reusable, styled component)</h1>
+    <h1>{{ $t('pageTitles.settings') }}</h1>
+    <p>Example computed string. It should say "today": {{ exampleComputedString }}</p>
+   <p>This is the settings page (TODO: Refactor into a reusable, styled component)</p>
   </div>
 </template>
 
 <script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+import { computed } from 'vue'
+
+const { t } = useI18n()
+
+const exampleComputedString = computed(() => {
+  if (true === true) return t('pageTitles.today')
+})
 </script>
