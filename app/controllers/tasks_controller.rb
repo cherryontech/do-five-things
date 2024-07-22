5705ad437fc99739c5a2fd0d151862993d7c4325
mashiinka <mariyamegovela@gmail.com>
2024-07-22 15:17:34 -0700

    update a task's complete status

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,9 +11,24 @@ class TasksController < ApplicationController
     end
   end
 
+  def edit
+    @task = resource
+    @task.update(complete: !@task.complete)
+    
+    if @task.save
+      redirect_to root_path
+    else
+      redirect_to root_path, status_code: 422, inertia: { errors: @task.errors }
+    end
+  end
+
   private
 
+  def resource
+    Task.find(resource_params[:task_id])
+  end
+
   def resource_params
-    params.require(:task).permit(:name, :order)
+    params.require(:task).permit(:name, :order, :task_id)
   end
 end
