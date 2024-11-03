1433c9a6894dd0d09ab0ad6993faf73f1bb5ee52
novellac <38117965+novellac@users.noreply.github.com>
2024-11-03 15:20:00 -0500

    feat: Add validaton and localStorage saving

---
+++ app/frontend/components/BaseButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <button class="py-3 px-12 text-2xl border-2 rounded-full transition ease-in-out focus:text-dft-black active:text-dft-black active:bg-dft-primary hover:text-dft-black hover:bg-dft-primary"
+  :class="hasError ? 'text-dft-error border-dft-error' : 'text-dft-primary border-dft-primary'"
+  >
+    <slot/>
+  </button>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  hasError: {
+    type: Boolean,
+    default: false
+  }
+})
+</script>
