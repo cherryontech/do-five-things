f4828873532595889d923b17958ca34c54f840e6
novellac <38117965+novellac@users.noreply.github.com>
2024-11-10 14:15:26 -0500

    feat: Add logic and styling for both lists on the today page

--- app/frontend/pages/TodayPage.vue
+++ app/frontend/pages/TodayPage.vue
@@ -1,14 +1,30 @@
 <template>
-  <div>
-    <h1>{{ $t('pageTitles.today') }}</h1>
+  <div class="mx-auto max-w-sm">
+    <h1 class="mb-24 text-4xl text-center">{{ $t('pageTitles.today') }}</h1>
+
     <h2 class="mb-16 text-2xl text-center">
+      <template v-if="!tasks">
       {{ $t('emptyStates.todayPage.text1') }}
       <Link class="dft-prose-link" href="/settings">{{ $t('emptyStates.todayPage.link1') }}</Link>
       {{ $t('emptyStates.todayPage.text2') }}
+      </template>
+
+      <template v-else>
+        {{ $t('flavorText.youGotThis') }}
+      </template>
     </h2>
 
-    <ol class="dft-list-layout">
-      <li v-for="task in sortedTasks" :key="task.id">
+    <h3><span class="text-2xl">{{ $t('labels.toDo') }}</span> {{ incompleteTasks.length }}/{{ sortedTasks.length }}</h3>
+    <ol v-if="incompleteTasks.length" class="my-6 dft-list-layout">
+      <li v-for="task in incompleteTasks" :key="task.id">
+        <TaskCheck :task="task" :isChecked="task.completed" @click="updateIsChecked(task.id)" />
+      </li>
+    </ol>
+    <p class="ml-4 my-6" v-else>{{ $t('emptyStates.nothingToSee') }}</p>
+
+    <h3><span class="text-2xl">{{ $t('labels.completed') }}</span> {{ completeTasks.length }}/{{ sortedTasks.length }}</h3>
+    <ol class="my-6 dft-list-layout">
+      <li v-for="task in completeTasks" :key="task.id">
         <TaskCheck :task="task" :isChecked="task.completed" @click="updateIsChecked(task.id)" />
       </li>
     </ol>
@@ -23,18 +39,19 @@ import TaskCheck from '../components/TaskCheck.vue';
 import { computed, reactive } from 'vue'
 import { Task } from '../types'
 
-const tasks: Task[] = reactive([
-  { id: 939, order: 3, completed: true, text: "Wash my face" },
-  { id: 297, order: 4, completed: false, text: "Make my bed and do another thing but this other thing might grab my attention so I will try to do it too" },
-  { id: 584, order: 2, completed: false, text: "Shower!" },
-  { id: 837, order: 5, completed: true, text: "Empty litter" },
-  { id: 102, order: 1, completed: false, text: "Brush teeth" }
-])
+const tasks: Task[] = reactive(localStorage.getItem('dftTasks') ? JSON.parse(localStorage.getItem('dftTasks')!) : [])
 
 const sortedTasks = computed(() => {
   return tasks.sort((a,b) => { return a.order - b.order })
 })
 
+const incompleteTasks = computed(() => {
+  return sortedTasks.value.filter(task => !task.completed)
+})
+
+const completeTasks = computed(() => {
+  return sortedTasks.value.filter(task => task.completed)
+})
 
 const updateIsChecked = (taskId: Task['id']) => {
   const currentTask = tasks.find(el => el.id === taskId)
@@ -42,5 +59,7 @@ const updateIsChecked = (taskId: Task['id']) => {
   if(currentTask) {
     currentTask.completed = !currentTask.completed
   }
+
+  localStorage.setItem('dftTasks', JSON.stringify(sortedTasks.value))
 }
 </script>
