a88d31c90ac4e59b5c6eedcaed6b7d062e1f99f6
mashiinka <mariyamegovela@gmail.com>
2024-11-21 10:31:17 -0800

    update migrations

--- db/migrate/20240623170320_create_tasks.rb
+++ db/migrate/20240623170320_create_tasks.rb
@@ -1,9 +1,10 @@
 class CreateTasks < ActiveRecord::Migration[7.1]
   def change
     create_table :tasks do |t|
-      t.integer :order
-      t.text    :name
-      t.boolean :complete
+      t.integer     :order
+      t.text        :text
+      t.boolean     :completed
+      t.datetime    :completed_at
 
       t.timestamps
     end
