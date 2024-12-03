6e91ef8125192f03492a637c3b3b572861301ee9
mashiinka <mariyamegovela@gmail.com>
2024-12-03 13:25:33 -0800

    Update task on complete

--- app/controllers/tasks/today_controller.rb
+++ app/controllers/tasks/today_controller.rb
@@ -6,19 +6,19 @@ module Tasks
 
     def edit
       @task = resource
-      @task.update(completed: !@task.completed)
+      @task.update(completed: params[:task][:completed])
 
       if @task.save
-        redirect_to root_path
+        redirect_to today_path
       else
-        redirect_to root_path, status_code: 422, inertia: { errors: @task.errors }
+        redirect_to today_path, status_code: 422, inertia: { errors: @task.errors }
       end
     end
 
     private
 
     def resource
-      Task.find(params[:task_id])
+      Task.find(params[:task][:id])
     end
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,10 +2,19 @@ class Task < ApplicationRecord
   validates     :text, presence: true
   before_create :validate_task_limit
   after_create  :validate_task_create_date
+  before_save   :check_completed_at
 
   scope :today, -> { where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day) }
 
   private
+
+  def check_completed_at
+    if completed_changed? && completed?
+      self.completed_at = Time.current
+    elsif completed_changed? && !completed?
+      self.completed_at = nil
+    end
+  end
 
   def validate_task_create_date
     return unless created_at < Time.zone.now.beginning_of_day
