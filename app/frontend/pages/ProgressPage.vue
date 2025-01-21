24a5012647b5c607bf6945807619bd7556ebb931
novellac <38117965+novellac@users.noreply.github.com>
2025-01-21 08:36:12 -0500

    feat: Add empty message for progress page

--- app/frontend/pages/ProgressPage.vue
+++ app/frontend/pages/ProgressPage.vue
@@ -1,9 +1,17 @@
 <template>
-  <div>
-    <h1>{{ $t('pageTitles.progress') }}</h1>
-   <p>This is the progress page</p>
+  <div class="mx-auto max-w-sm">
+    <h1 class="mb-24 text-4xl text-center">{{ $t('pageTitles.progress') }}</h1>
+    <p class="mb-16 text-xl text-center">
+      {{ $t('emptyStates.progressPage.text1') }}
+      <Link class="dft-prose-link" href="/today">
+        {{ $t('emptyStates.progressPage.link1') }}
+      </Link>
+      {{ $t('emptyStates.progressPage.text2') }}
+    </p>
   </div>
 </template>
 
 <script setup lang="ts">
+import { Link, router } from '@inertiajs/vue3'
+
 </script>
