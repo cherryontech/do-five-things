f6ecf8db73c08628ea7d3782aa0690178c8b3baf
mashiinka <mariyamegovela@gmail.com>
2025-02-11 11:01:29 -0800

    add date to task_progs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_11_183001) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_11_185952) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,6 +39,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_11_183001) do
     t.datetime "completed_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "date"
     t.index ["daily_prog_id", "task_id"], name: "index_task_progs_on_daily_prog_id_and_task_id", unique: true
     t.index ["daily_prog_id"], name: "index_task_progs_on_daily_prog_id"
     t.index ["task_id"], name: "index_task_progs_on_task_id"
