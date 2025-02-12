cf1370d933f819baa9d8c67f5a5df79f334bd734
mashiinka <mariyamegovela@gmail.com>
2025-02-11 16:22:25 -0800

    WIP update to new structure

--- app/controllers/tasks/today_controller.rb
+++ app/controllers/tasks/today_controller.rb
@@ -1,14 +1,23 @@
 module Tasks
   class TodayController < ApplicationController
     def index
-      render inertia: 'TodayPage', props: { tasks: Task.today }
+      goal = Goal.first.presence || Goal.create!(start_date: Date.current)
+      tasks = goal.tasks
+      daily_prog = DailyProg.find_or_initialize_by(goal: goal, date: Date.current)
+      daily_prog.save! if daily_prog.new_record?
+
+      
+
+      render inertia: 'TodayPage', props: { tasks: tasks }
+    rescue StandardError => e
+      redirect_to today_path, status_code: 422, inertia: { errors: e.message }
     end
 
     def edit
       @task = resource
       @task.update(completed: params[:task][:completed])
 
-      if @task.save
+      if @task.valid
         redirect_to today_path
       else
         redirect_to today_path, status_code: 422, inertia: { errors: @task.errors }
