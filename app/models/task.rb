2a5533ee4b515b32eb66d80800fc02a4c947dcc1
mashiinka <mariyamegovela@gmail.com>
2025-02-11 16:21:56 -0800

    update task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,30 +1,11 @@
 class Task < ApplicationRecord
-  validates     :text, presence: true
-  before_create :validate_task_limit
-  after_create  :validate_task_create_date
-  before_save   :check_completed_at
+  # validates     :text, presence: true
+  validates     :order, presence: true
 
-  scope :today, -> { where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day) }
+  has_many      :task_progs
+  belongs_to    :goal
 
-  private
-
-  def check_completed_at
-    if completed_changed? && completed?
-      self.completed_at = Time.current
-    elsif completed_changed? && !completed?
-      self.completed_at = nil
-    end
-  end
-
-  def validate_task_create_date
-    return unless created_at < Time.zone.now.beginning_of_day
-
-    errors.add(:base, "Tasks cannot be created for past dates.")
-  end
-
-  def validate_task_limit
-    return unless Task.today.count >= 5
-
-    errors.add(:base, "There can be at most 5 tasks for the day")
+  def daily_task_prog
+    task_progs.where(date: Date.current)&.first
   end
 end
