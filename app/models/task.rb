5c8207e7eea309401b74d5620b4f4bcdd7094c46
mashiinka <mariyamegovela@gmail.com>
2025-02-12 17:03:37 -0800

    destroy task_progs when tasks are destroyed

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,7 @@ class Task < ApplicationRecord
   # validates     :text, presence: true
   validates     :order, presence: true
 
-  has_many      :task_progs
+  has_many      :task_progs, dependent: :destroy
   belongs_to    :goal
 
   def daily_task_prog
