26785bd770e5f3f8e8e44d4493fafe9c0209e7d5
mashiinka <mariyamegovela@gmail.com>
2025-02-15 18:54:06 -0800

    fix task object

--- app/decorators/task_decorator.rb
+++ app/decorators/task_decorator.rb
@@ -11,9 +11,9 @@ class TaskDecorator < Draper::Decorator
   #   end
   def as_json_for_today
     {
-      task_id: id,
-      task_order: order,
-      task_text: text,
+      id: id,
+      order: order,
+      text: text,
       completed: daily_task_prog.completed?,
       completed_at: daily_task_prog.completed_at
     }
