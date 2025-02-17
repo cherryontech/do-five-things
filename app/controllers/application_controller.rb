fadf9deeccc847faa59b715d4056dcbbc55e9a49
mashiinka <mariyamegovela@gmail.com>
2025-02-16 16:11:30 -0800

    initialize progs once for all pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :initialize_progs
+
+  private
+
+  def initialize_progs
+    today = Date.current.to_s
+    return if session[:initialize_progs] == today
+
+    ProgService.initialize_progs
+    session[:initialize_progs] = today
+  end
 end
--- app/services/task_service.rb
+++ app/services/task_service.rb
@@ -1,8 +1,6 @@
 class TaskService
   def self.fetch_today_tasks
-    goal = Goal.first.presence || Goal.create!(start_date: Date.current)
-    daily_prog = DailyProg.find_or_initialize_by(goal: goal, date: Date.current)
-    daily_prog.save! if daily_prog.new_record?
+    goal = Goal.last
     goal.tasks.decorate.map(&:as_json_for_today)
   end
 end
