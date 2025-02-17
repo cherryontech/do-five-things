8b3a08150f3bb374467dbc8c1a8f364900c62fbc
mashiinka <mariyamegovela@gmail.com>
2025-02-16 16:45:01 -0800

    fix tasks table

--- db/migrate/20250208190233_change_tasks_table.rb
+++ db/migrate/20250208190233_change_tasks_table.rb
@@ -1,8 +1,19 @@
 class ChangeTasksTable < ActiveRecord::Migration[7.1]
-  def change
+  def up
     change_table :tasks do |t|
       t.remove          :completed, :completed_at
-      t.references      :goal, null: false, foreign_key: true
+      t.references      :goal, null: true, foreign_key: true
+    end
+
+    Task.where(goal_id: nil).update_all(goal_id: 1)
+    change_column_null :tasks, goal_id: false
+  end
+
+  def down
+    change_table :tasks do |t|
+      t.remove_references :goal
+      t.boolean :completed
+      t.datetime :completed_at
     end
   end
 end
