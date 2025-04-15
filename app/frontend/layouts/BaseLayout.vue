92bf31db82f696f21d78ec91791ddea38c128293
novellac <38117965+novellac@users.noreply.github.com>
2025-04-14 21:31:34 -0400

    chore: Hide skiplink till focused

--- app/frontend/layouts/BaseLayout.vue
+++ app/frontend/layouts/BaseLayout.vue
@@ -1,13 +1,13 @@
 <template>
   <main class="flex flex-col min-h-screen p-4 appBg">
-    <header class="border-4">
-      <a href="#footerNav">Skip to nav (TODO: Hide until focused with keyboard)</a>
+    <header>
+      <a href="#footerNav" class="sr-only focus:not-sr-only">{{ $t('nav.skipLink') }}</a>
     </header>
     <article class="grow">
 
       <slot />
     </article>
-    <footer id="footerNav" class="rounded-t-xl bg-dft-secondary">
+    <footer id="footerNav" class="rounded-t-xl bg-dft-secondary" tabindex="-1">
       <ul class="flex justify-center">
         <li class="basis-36 max-w-36">
           <NavLink linkDest="/settings" icon="cog">
