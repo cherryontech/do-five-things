32e3f29c19e59ce0c22ddada73844f27de5f8566
mashiinka <mariyamegovela@gmail.com>
2025-05-11 09:57:52 -0700

    update to reflect goal user relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,17 @@ class User < ApplicationRecord
             format: { with: URI::MailTo::EMAIL_REGEXP } # <-- validates that the email is in correct format, and that it is unique
 
   passwordless_with :email # <-- tells Passwordless which field stores the email address
+
+  def current_goal
+    goals.order(created_at: :desc).first || create_default_goal
+  end
+
+  private
+
+  def create_default_goal
+    goals.create!(
+      start_date: Date.current,
+      end_date: Date.current + 182.days
+    )
+  end
 end
--- app/services/prog_service.rb
+++ app/services/prog_service.rb
@@ -1,7 +1,6 @@
 class ProgService
   def self.initialize_progs
-    goal = Goal.last.presence || Goal.create!(start_date: Date.current)
-    daily_prog = DailyProg.find_or_initialize_by(goal: goal, date: Date.current)
-    daily_prog.save! if daily_prog.new_record?
+    goal = current_user.current_goal
+    DailyProg.create_or_find_by!(goal: goal, date: Date.current)
   end
 end
